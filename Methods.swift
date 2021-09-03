3a1679e2775c8f1e99a85828e275d7a54a290908
Bachiero <bitsadze.bachuki@gmail.com>
2021-09-03 19:02:25 +0400

    first commit

---
+++ Methods.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  Movies
+//
+//  Created by Bacho Bitsadze on 03.09.21.
+//
+import UIKit
+
+
+func RegisterCollectionViewCell(collectionView:  UICollectionView) {
+    
+    
+    collectionView.register(<#T##cellClass: AnyClass?##AnyClass?#>, forCellWithReuseIdentifier: <#T##String#>)
+}
--- Movies/Grid Screen/ViewController.swift
+++ Movies/Grid Screen/ViewController.swift
@@ -7,13 +7,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GridViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    
 
 }
 

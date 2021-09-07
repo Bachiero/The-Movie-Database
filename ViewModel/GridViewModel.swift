d6a6d3185c3ab62ad187fd01a1f0f2f4c2901177
Bachiero <bitsadze.bachuki@gmail.com>
2021-09-07 15:45:36 +0400

    grid with pictures

--- ViewModel/GridViewModel.swift
+++ ViewModel/GridViewModel.swift
@@ -1,14 +1,47 @@
-//
-//  GridViewModel.swift
-//  Movies
-//
-//  Created by Bacho Bitsadze on 04.09.21.
-//
+import UIKit
 
-import Foundation
-
-class GridViewModel {
+class GridViewController: UIViewController {
+    
+   
+    
+    private var model: [String]?
+    var randomPictures = ["car1", "cat1", "panda1","car1", "cat1", "cat1", "panda1","car1", "cat1", "cat1", "panda1","car1", "cat1", "cat1", "panda1","car1", "cat1"]
+    
+    @IBOutlet var favButton: UIButton?
+    @IBOutlet var filterButton: UIButton?
+    @IBOutlet var gridCollectionView: UICollectionView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        model = randomPictures
+        
+        favButton?.tintColor = UIColor.cyan
+        
+        gridCollectionView?.register(UINib(nibName: "GridCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "GridCollectionViewCell")
+        gridCollectionView?.dataSource = self
+        gridCollectionView?.delegate = self
+        
+        
+        
+    }
     
     
+}
+
+extension GridViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return model?.count ?? 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        guard let model = model else { return UICollectionViewCell() }
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: GridCollectionViewCell.identifier, for: indexPath) as! GridCollectionViewCell
+        cell.configure(with: model[indexPath.row])
+        return cell
+    }
     
 }
